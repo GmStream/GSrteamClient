20e8f3ac43efd49d2b28cdcec1cbb6ed97204a62
Elijah <ilya.bobr.thj@gmail.com>
2018-01-19 16:49:12 +0300

    configured tests

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { applyMiddleware, compose, createStore, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
+import Hello from './Hello/';
 
 import * as ReactDOM from 'react-dom';
 
@@ -13,15 +14,11 @@ declare var process: any;
 const App = () => (
   <div className="wrapper">
     <div className="page-content">
-      <Switch>{/* here comes routes */}</Switch>
+      <Switch>
+        <Route exact path="/" component={Hello} />
+      </Switch>
     </div>
   </div>
 );
 
-ReactDOM.render(<h1>dfgdfgdfgdfg</h1>, document.getElementById('app'));
-
-//   <BrowserRouter>
-//     <Provider store={}>
-//       <Route component={} />
-//     </Provider>
-//   </BrowserRouter>,
+ReactDOM.render(<Hello />, document.getElementById('app'));
