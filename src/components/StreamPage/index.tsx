76295f49a022afe3ed0a5f1d2cf27f7c3215f696
Elijah <ilya.bobr.thj@gmail.com>
2018-02-20 18:31:42 +0300

    reconfigure player

--- src/components/StreamPage/index.tsx
+++ src/components/StreamPage/index.tsx
@@ -16,13 +16,24 @@ class StreamPage extends React.PureComponent<IProps> {
   public componentDidMount() {
     const div = document.createElement(`div`);
     div.setAttribute('id', 'player');
-    document.body.appendChild(div);
+
+    const container: any = document.getElementById('Pcontainer');
+    container.appendChild(div);
+
+    let screenWidth = window.screen.width;
+    let height = screenWidth / 1.5;
+
+    if (screenWidth > 630) {
+      screenWidth = 630;
+      height = 360;
+    }
+
     const evalString = `
     hdwplayer({
       id       : 'player',
       swf      : '../hdwPlayer/player/player.swf',
-      width    : '630',
-      height   : '360',
+      width    : ${screenWidth},
+      height   : ${height},
       type     : 'rtmp',
       streamer : '${config.STREAM_SERVER}',
       video    : '${this.props.appData.selectedStreamId}',
@@ -43,7 +54,11 @@ class StreamPage extends React.PureComponent<IProps> {
   }
 
   public render() {
-    return <div className="stream_page" />;
+    return (
+      <div className="stream_page">
+        <div id="Pcontainer" className="player_container" />
+      </div>
+    );
   }
 }
 
