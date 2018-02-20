77dc6a4201b160435e3b0623fe2d67b451756d65
Elijah <ilya.bobr.thj@gmail.com>
2018-02-20 18:13:24 +0300

    add socket method

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,10 +4,19 @@ import * as interfaces from '../models/interfaces';
 const API = 'localhost:2999';
 
 import * as io from 'socket.io-client';
-const socket = io.connect(`http://localhost:2999/`);
+
+export const socket = io.connect(`http://localhost:2999/`);
 
 export const signUp = (payload: any) => axios.post(`http://${API}/api/user/signup`, payload);
 
 export const signIn = (payload: any) => axios.post(`http://${API}/api/user/signin`, payload);
 
 export const confirm = (payload: any) => axios.post(`http://${API}/api/user/confirm`, payload);
+
+export const connectToChatRoom = (payload: any) => {
+  socket.emit('connect to room', payload);
+};
+
+export const sendMessage = (payload: any) => {
+  socket.emit('message', payload);
+};
